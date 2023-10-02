21ea314c0f4484e9de98d88dba5bd80d538c6961
yyang31 <stanley316868@gmail.com>
2023-10-01 23:36:59 -0700

    add text box for traditional site

--- traditional.js
+++ traditional.js
@@ -1,6 +1,11 @@
 // common selectors
 let screenshotButtonSelector = document.getElementById("screenshotButton");
 let stopButtonSelector = document.getElementById("stopButton");
+let menuBarContainerSelector = document.getElementById("menuBarContainer");
+let traditionalSkillTreeContainerSelector = document.getElementById(
+    "traditionalSkillTreeContainer"
+);
+let skillTextAreaSelector = document.getElementById("skillTextArea");
 
 function stopSkilltree() {
     stopButtonSelector.style.display = "none";
@@ -18,12 +23,23 @@ function stopSkilltree() {
     }, 1000);
 }
 
+function auto_grow() {
+    skillTextAreaSelector.style.height = "45px";
+    skillTextAreaSelector.style.height =
+        skillTextAreaSelector.scrollHeight + "px";
+
+    traditionalSkillTreeContainerSelector.style.paddingTop =
+        skillTextAreaSelector.getBoundingClientRect().bottom + "px";
+}
+
 window.onload = () => {
     // check for mobile device
     mobileAndTabletCheck();
     if (hasLoadingError) return;
 
     generateId(6);
+
+    auto_grow();
 
     // hide loading screen
     setTimeout(function () {
